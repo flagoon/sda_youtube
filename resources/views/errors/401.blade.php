547f3c5900090e59f8167875db1456fa27b71d7a
Paweł Kochanek <flagoon@o2.pl>
2018-04-23 21:14:31 +0200

    Stubs for admin and user homepage, some small corrections.

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -2,6 +2,6 @@
 @section('content')
 <div class="container alert alert-danger">
     <p>You don't have permission to see this page</p>
-    <a href={{ route('home') }}><button class="btn btn-primary">Back</button></a>
+    <a href={{ url('/') }}><button class="btn btn-primary">Back</button></a>
 </div>
 @endsection
