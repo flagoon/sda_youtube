0404faa1a4f89df8996a5f4331bc3f64fa11ac8c
Paweł Kochanek <flagoon@o2.pl>
2018-04-23 20:35:45 +0200

    Added error page for better handling aborts from authentication.

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+<div class="container alert alert-danger">
+    <p>You don't have permission to see this page</p>
+    <a href={{ route('home') }}><button class="btn btn-primary">Back</button></a>
+</div>
+@endsection
