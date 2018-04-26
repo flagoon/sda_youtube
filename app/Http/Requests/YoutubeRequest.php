d81d55a60d5bdce40de0caab06eadf14a179c27b
Paweł Kochanek <flagoon@o2.pl>
2018-04-26 22:05:48 +0200

    Created own request for form validation.

---
+++ app/Http/Requests/YoutubeRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class YoutubeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'film_link' => ['required', 'regex:/www.youtube.com/']
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'film_link.required' => 'Can\'t add movie without a youtube link!',
+            'film_link.regex' => 'It\' not a valid youtube link!'
+        ];
+    }
+}
